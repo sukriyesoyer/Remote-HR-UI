5a0fb190e5b2466d4bd76dd53bc4f6daa5fea513
erkanplt <65369334+erkanplt@users.noreply.github.com>
2020-08-11 10:24:19 +0300

    Klasör düzenlemesi

--- spring-boot/birthdayCalendar/src/main/java/com/birthday/repository/BirthdayRepository.java
+++
@@ -1,9 +0,0 @@
-package com.birthday.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.birthday.model.Birthday;
-
-public interface BirthdayRepository extends JpaRepository<Birthday, Long> {
-
-}
